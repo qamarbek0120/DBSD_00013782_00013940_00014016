d3b66f04f3fc733e144a5a1c735a6c6d8f3b6f58
Qamarbek Safarbayev <qamarbeksafarbayev1@gmail.com>
2024-03-27 14:36:35 +0500

    Add project files.

---
+++ DBSD_00013782_00013940_00014016/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DBSD_00013782_00013940_00014016.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DBSD_00013782_00013940_00014016.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DBSD_00013782_00013940_00014016/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DBSD_00013782_00013940_00014016.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
